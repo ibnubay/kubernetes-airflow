26476d9c2d55a27064f6706c5dc9281a03b0f2de
ibnbay99 <ibnubayhaqi99@gmail.com>
2020-06-27 23:55:23 +0700

    edit uninstall_app.sh

--- persistent_volume/uninstall_app.sh
+++ persistent_volume/uninstall_app.sh
@@ -2,8 +2,8 @@
 NAMESPACE=$1
 
 microk8s kubectl delete -f app-postgresql.yaml --namespace=${NAMESPACE}
-#microk8s kubectl delete -f app-minio.yaml --namespace=${NAMESPACE}
+microk8s kubectl delete -f app-minio.yaml --namespace=${NAMESPACE}
 microk8s kubectl delete -f app-airflow.yaml --namespace=${NAMESPACE}
-#microk8s kubectl delete -f app-ingress.yaml --namespace=${NAMESPACE}
-#microk8s kubectl delete -f app-volume.yaml --namespace=${NAMESPACE}
-#microk8s kubectl delete namespace ${NAMESPACE} || true
+microk8s kubectl delete -f app-ingress.yaml --namespace=${NAMESPACE}
+microk8s kubectl delete -f app-volume.yaml --namespace=${NAMESPACE}
+microk8s kubectl delete namespace ${NAMESPACE} || true
